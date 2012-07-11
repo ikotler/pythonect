547a7c49ea44f1a088065d6659f7bcd3d4bb8f52
Itzik Kotler <xorninja@gmail.com>
2012-07-11 23:59:14 +0300

    Fix #25 by ikotler: Unload modules from __init__ after execution to avoid polluting the package namespace

--- pythonect/__init__.py
+++ pythonect/__init__.py
@@ -4,8 +4,16 @@ try:
 
     from _version import __version__
 
+    # Clean up namespace
+
+    del _version
+
 except ImportError, e:
 
     from internal.version import get_version
 
     __version__ = get_version()
+
+    # Clean up namespace
+
+    del e, get_version, internal
